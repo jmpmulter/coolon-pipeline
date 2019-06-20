8ccd0e338503fdb8b97e64712d26b92eac4fd518
jmpmulter <jmpmulter@gmail.com>
2019-06-20 17:01:49 -0400

    deleted a scrap version of the csv_format.py file

--- csv_processor.py
+++
@@ -1,7 +0,0 @@
-##Process csv files output by the pipeline into analyzeable files
-import os
-
-#open csv
-#open procesed_csv
-#if GM segment exists, take GM segment out of line and add that to a new column
-#?Calculate ratio?
