1caeb0abe2c76acce5e262b4a4ca0b95112c0c44
jmpmulter <jmpmulter@gmail.com>
2019-06-20 17:53:39 -0400

    changed formatting to be correct. Also placed decimal formatting of the float.

--- csv_formatter.py
+++ csv_formatter.py
@@ -15,14 +15,14 @@ def main():
     in_path = instr[1]
     out_path = instr[2]
     
-    if(runmode !=4):
+    if(runmode !="4"):
         if(in_path == ""):
             in_path = input("Enter the name of or relative path to the file to format:\t")
         if(out_path == ""):
             out_path = input("Enter the name of or relative path to the outputted File:\t")
         process_csv(runmode,in_path,out_path)
     else: #Run on all files in directory
-        if in_path = "":
+        if in_path == "":
             pass
         else:
             os.chdir(in_path)
@@ -42,11 +42,12 @@ def process_csv(runmode,in_path,out_path):
     infile = open(in_path, 'r')
     outfile = open(out_path, 'x')
     buff_ln = "" #String which represents the next line to be written to outfile
-    in_header = f.readline().strip()
+    in_header = infile.readline().strip()
     out_header = in_header+",Name,ID,nCounts,Ratio_Ref_div_Alt, Ratio_Alt_div_Ref\n"
     outfile.write(out_header)
     
     for line in infile:
+    
         buff_ln = ""
         #no_ratio = False #flipped to true if 
         l0 = line
@@ -55,8 +56,10 @@ def process_csv(runmode,in_path,out_path):
         cAlt = int(l0s[4].strip())
         nCounts = str(cRef+cAlt) #total number of counts
         info = l0s[5].strip().split(";")# information from geneinfo column
+        
         ID = info[0].split(":")[1].strip() #Generally a FBGN Number
-        Name = info[1].splint("=")[1].strip() #Generally GM......
+
+        Name = info[1].split("=")[1].strip() #Generally GM......
         
         ratio_r_over_a = ""
         ratio_a_over_r = ""
@@ -65,14 +68,21 @@ def process_csv(runmode,in_path,out_path):
             ratio_r_over_a = "NA"
             ratio_a_over_r = "NA"  
         else:
-            ratio_r_over_a = str(cRef/cAlt)
-            ratio_a_over_r = str(cAlt/cRef)
+            rda = cRef/cAlt
+            adr = cAlt/cRef
+            ratio_r_over_a = str(format(rda,'.4f'))
+            ratio_a_over_r = str(format(adr,'.4f'))
         
+        print(Name)
+        print(ID)
         to_add = [Name,ID,nCounts,ratio_r_over_a,ratio_a_over_r]
-        buff_ln = l0.strip()+",".join(to_add)+"\n"#TODO ADD ITEMS IN SAME ORDER AS HEADER
-        
+        for item in to_add:
+            print(item)
+        buff_ln = l0.strip()+","+",".join(to_add)+"\n"#TODO ADD ITEMS IN SAME ORDER AS HEADER
+        print(buff_ln)
+        print("\n\n")
         outfile.write(buff_ln)
-
+#scaffold_3174,1865,./intermeds/NVC_a_type1_OA_Galstep99_ON_DATA_22_98_FILT_GENES.txt,79,13,ID=gene:FBgn0171184;Name=GM16268;biotype=protein_coding;gene_id=FBgn0171184;logic_name=flybaseGM16268,FBgn0171184,92,6.076923076923077,0.16455696202531644
     infile.close()
     outfile.close()
 
@@ -80,11 +90,11 @@ def process_runline():
     """Reads sys.argv and recomposes it as a list. This list sets out what information the program has or will ask for from console input.
         Full input key:
         sys.argv[0]->The Script's name
-        sys.argv[1]-> runmode. No arg or 1 == command line input, 2==input file path, 3 == output file path, 4 all CSV files in directory
+        sys.argv[1]-> runmode. Default mode is 4, or 1 == user input, 2== command line has file path, 3 == command line has output file path, 4 all CSV files in directory
         sys.argv[2]-> input file path. If runmode ==4, this can be a target directory; else the default is current directory
         sys.argv[3]-> output file path
     """
-    runmode = 0
+    runmode = 4
     in_path = ""
     out_path = ""
     if len(sys.argv) == 2:
