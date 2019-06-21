8dde699ecb4ea889619055846dd0585b79b0a2fb
jmpmulter <jmpmulter@gmail.com>
2019-06-21 13:23:44 -0400

    bugfixes; now functional

--- generate_target.py
+++ generate_target.py
@@ -1,15 +1,18 @@
 #generate_target.py
 #Generates a list of target genes from a flybase output
+import os
+import sys
+
 def main():
-    in_path = sysargv[1]
-    out_path = sysargv[2]
+    in_path = sys.argv[1]
+    out_path = sys.argv[2]
     infile = open(in_path, "r")
     outfile = open(out_path, "x")
     for l0 in infile:
         l0s = l0.split(",")
         for item in l0s:
             if "Dsec" in item:
-                outfile.write(l0s.split("\\")[1]+"\n")
+                outfile.write(item.split("\\")[1]+"\n")
 
 if __name__ == "__main__":
     main()
