47becb07e6eb88ec737fe96bf3e1b9173627b2f8
jmpmulter <jmpmulter@gmail.com>
2019-06-21 16:30:16 -0400

    debugging

--- gene_scan.py
+++ gene_scan.py
@@ -34,13 +34,60 @@ def main():
     FO_csv_list = []
     for path in all_paths: #filter so only the Filtered files are being considered
         if ("FO" in path):
-            print("Detected .csv file with path:\t"+path)
-            csv_list.append(path)
-        for csv in csv_list:
-            out_path = "FO_"+csv
-            print("Processing with inputs:\t4\t"+in_path+"\t"+out_path)
-            process_csv(4,csv,out_path)
+            print("Detected Filtered .csv file with path:\t"+path)
+            FO_csv_list.append(path)
     
+    scan_runner(FO_csv_list,target)
+            
+            
+def scan_runner(FO_csv_list,target):
+    for file in FO_csv_list:
+        out_path = "SCAN_"+target+"_"+file
+        scan_FO(target,file,out_path)
+        
+def scan_FO(target,in_path,out_path):
+    """Searches an in_path csv file for any of the genes in target. Escapes on the first instance. Only gives whether or not those genes exist, prints total number of hits/total
+
+    """
+    tfile = open(target, "r")
+    infile = open(in_path,"r")
+    outfile = open(out_path,"x")
+    list_hits = []
+    i = 0
+    j = 0
+    
+    for line in tfile:
+        Name = line.strip()#line.split(",")[0].strip()
+        j+=1
+        #skip header of infile #TODO URG
+        infile.seek(0) #resets cursor to the beginning of the file.
+        in_header = infile.readline().strip()
+        #print(line)
+        for l1 in infile:
+            l1_name = l1.split(",")[6].strip()
+            #print("CHECK")
+            if Name == l1_name:
+                #print("HIT")
+                if (Name in list_hits):
+                    pass
+                    print("Hit already Detected")
+                else:
+                    list_hits.append(Name)
+                    i+=1
+                
+                
+    
+    #get the info out of the dictionary, sort it, write it
+    
+    sorted_hits = sorted(list_hits)
+    #print hits/target
+    for hit in sorted_hits:
+        outfile.write(hit+",\n")
+            
+    tfile.close()
+    infile.close()
+    outfile.close()
+    print(str(i)+"/"+str(j)+"\t hits/targets in\t"+out_path)
     
 if __name__ == "__main__": #sets up a main area. This will not work well if imported
     main()
